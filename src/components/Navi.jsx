b81889ad2b7b49000b12af6fc9d30a8a1db41956
viknik08 <63363344+viknik08@users.noreply.github.com>
2020-07-12 17:30:14 +0800

    new

---
+++ src/components/Navi.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navi = () => {
+	return (
+		<nav className="navi">
+			<div>
+				<a>Profile</a>
+			</div>
+			<div>
+				<a>Messages</a>
+			</div>
+			<div>
+				<a>News</a>
+			</div>
+			<div>
+				<a>Music</a>
+			</div>
+			<div>
+				<a>Sittings</a>
+			</div>
+		</nav>
+	)
+}
+
+export default Navi
